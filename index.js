70059e9706ff2be98416abb828165425ae5180ef
apaezp <andrea.paez86@gmail.com>
2022-12-12 21:00:36 -0300

    conditionals

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ if (operacion === "leer") {
   leer();
 };
 
+if(argumentos === undefined || argumentos.length === 0 || operacion !== "registrar" || operacion !== "leer" || !argumentos[1] || !argumentos[2] || !argumentos[3] || !argumentos[4] || !argumentos[5]) {
+  console.log("Por favor, ingrese una operación válida");
+  return;
+}
